b1af75cb76567ef5bcdadde81b42fb7c174cc098
Agustín Roca <aroca@itba.edu.ar>
2021-11-07 19:33:13 -0300

    R

---
+++ parejo.r
@@ -0,0 +1,27 @@
+data <- read.csv("parejo.csv", sep=";", colClasses=c("ID"="character"))
+print(data)
+
+library("ggplot2")
+theme_set(theme_bw())
+library("sf")
+
+library(geojsonio)
+gj <- geojson_read("geojson/ProvinciasArgentina.geojson", what = "sp")
+library(broom)
+tidied_gj <- tidy(gj)
+
+library(dplyr)
+tidied_gj = tidied_gj %>%
+    left_join(. , data, by=c("id"="ID"))
+
+bronze <- "#cd7f32"
+silver <- "#C0C0C0"
+gold <- "#d4af37"
+
+library(viridis)
+
+ggplot() +
+    geom_polygon(data = tidied_gj, aes( x = long, y = lat, group = group, fill=Parejo), color="white") +
+    scale_fill_viridis() +
+    theme_void() +
+    coord_map()
